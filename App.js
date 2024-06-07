695df88587dced3345146d5bfa2024d7cf686391
Tejaskadam20 <tejaskadam7267@gmail.com>
2024-06-07 15:32:48 +0530

    Intial Commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+    
+    <ul className="nav">
+  <li className="nav-item">
+    <a className="nav-link active" aria-current="page" href="/">Home</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="/">About</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="/">Skills</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="/">Contact</a>
+  </li>
+</ul>
+    </>
+  );
+}
+export default App;
+
